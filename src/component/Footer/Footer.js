4a9ed276b840bb04a671f4f213d2410832d42906
PIERRON Marc <pierronmarc@outlook.fr>
2020-03-21 13:36:30 +0100

    update: footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends Component {
     render() { 
         return (
-            <footer className="footer">
-                <ul>
-                    <li>github</li>
-                    <li>linkedin</li>
-                </ul>
-                <h3>&lt;/body&gt;</h3>
-                <h3>&lt;/html&gt;</h3>
+            <footer className="footer ">
+                
+                <div class="footer__tags">
+                    <h3 className="html-tags ml-10">&lt;/body&gt;</h3><br/>
+                    <h3 className="html-tags inline-block">&lt;/html&gt;</h3> 
+                </div>
+
+                <h3 className="html-tags inline-block footer__copyright">&lt;!-- Copyright @ 2020 PIERRON Marc --&gt;</h3>
+
+                <div className="footer__social-wrapper">
+                    <h3 className="html-tags inline-block">&lt;!-- Socials : </h3>
+                    <ul className="footer__social">
+                        <li className="footer__social-item"><FontAwesomeIcon icon={faLinkedin} /></li>
+                        <li className="footer__social-item"><FontAwesomeIcon icon={faGithubSquare} /></li>
+                    </ul>
+                    <h3 className="html-tags inline-block">--&gt;</h3>
+                </div>
             </footer>
         );
     }
--- src/component/Gallery/Gallery.js
+++ src/component/Gallery/Gallery.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
 import './gallery.scss';
+import Masonry from 'react-masonry-component';
+
+const masonryOptions = {
+    // transitionDuration: '0.4s'
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-col',
+    percentPosition: true
+
+};
+ 
+const imagesLoadedOptions = { background: '.my-bg-image-el' }
 
 class Gallery extends Component {
+
+    
     render() { 
+
         return (
-            <section>
-                <h1>Mes travaux</h1>
-                <h2>**mosaïque**</h2>
+            <section className="container gallery">
+                {/* <h1>Mes travaux</h1> */}
+                
+
+            <Masonry
+                className={'grid'} // default ''
+                elementType={'div'} // default 'div'
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+                imagesLoadedOptions={imagesLoadedOptions} // default {}
+            >
+
+            <div className="grid-col"></div>   
+            <div className="grid-col grid-col--2"></div>   
+            <div className="grid-col grid-col--3"></div>   
+            <div className="grid-col"></div>   
+
+            <div className="grid-item">
+                <div className="title-wrapper">
+                    <h1 className="title">Mes <span className="title--highlight">travaux</span></h1>
+
+                </div>
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/400x120" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x150" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x90" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x150" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x120" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x150" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x90" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x150" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x120" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x150" />
+            </div>
+            <div className="grid-item">
+                <img src="https://via.placeholder.com/290x90" />
+            </div>
+          
+               
+            </Masonry>
+
             </section>
         );
     }
