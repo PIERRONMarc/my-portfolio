e8e3a6c38c7b612e9866a4c85d58998ee5eeb0c0
PIERRON Marc <pierronmarc@outlook.fr>
2020-03-28 20:09:07 +0100

    Updates

---
+++ precache-manifest.7cebe50c63664b004a679d981443176f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c39dd2708755e63b120ae549594ec3",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "396af6c310ec9d93e90f",
+    "url": "/my-portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ab8e635c4ef534c9ef65",
+    "url": "/my-portfolio/static/css/main.ac423697.chunk.css"
+  },
+  {
+    "revision": "396af6c310ec9d93e90f",
+    "url": "/my-portfolio/static/js/2.1dc02098.chunk.js"
+  },
+  {
+    "revision": "70eb1b90ced0a78736a3f811e754d3e8",
+    "url": "/my-portfolio/static/js/2.1dc02098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8e635c4ef534c9ef65",
+    "url": "/my-portfolio/static/js/main.1c12a77f.chunk.js"
+  },
+  {
+    "revision": "c76e595a7e7cbc144a48",
+    "url": "/my-portfolio/static/js/runtime-main.40ca06be.js"
+  },
+  {
+    "revision": "98f858e9a3c7bccd6682fb7877dbedaf",
+    "url": "/my-portfolio/static/media/FNPEIS.98f858e9.PNG"
+  },
+  {
+    "revision": "d9b407da11c85b00d67cf7f70a52a5ca",
+    "url": "/my-portfolio/static/media/GIME.d9b407da.PNG"
+  },
+  {
+    "revision": "8d514cd15187e3ba2c8811d372cda5f4",
+    "url": "/my-portfolio/static/media/gestion-des-greves.8d514cd1.PNG"
+  },
+  {
+    "revision": "5b718a417ded53fd717bc7a2eb28b22e",
+    "url": "/my-portfolio/static/media/integration.5b718a41.PNG"
+  },
+  {
+    "revision": "1ebcdaedfd840d80ac3f56e73908ab4b",
+    "url": "/my-portfolio/static/media/labelleaurore-regular-webfont.1ebcdaed.woff2"
+  },
+  {
+    "revision": "67f11b1946c01124ff70fed0d5e89ddc",
+    "url": "/my-portfolio/static/media/labelleaurore-regular-webfont.67f11b19.woff"
+  },
+  {
+    "revision": "4525bdb465ba96525045d8e35c45f405",
+    "url": "/my-portfolio/static/media/mars-rover.4525bdb4.PNG"
+  },
+  {
+    "revision": "1e0a1604afda18a2f491d82b95b8a1f5",
+    "url": "/my-portfolio/static/media/montserrat-italic-webfont.1e0a1604.woff2"
+  },
+  {
+    "revision": "e95ccc21755714c42c4254bfd82c7909",
+    "url": "/my-portfolio/static/media/montserrat-italic-webfont.e95ccc21.woff"
+  },
+  {
+    "revision": "037e47dee96d2dcb48104e8c2dce549f",
+    "url": "/my-portfolio/static/media/montserrat-light-webfont.037e47de.woff2"
+  },
+  {
+    "revision": "8e166108285123d309ebb7c55fd24dd0",
+    "url": "/my-portfolio/static/media/montserrat-light-webfont.8e166108.woff"
+  },
+  {
+    "revision": "3fc3b912475708a963c77d4559a0a7dc",
+    "url": "/my-portfolio/static/media/montserrat-lightitalic-webfont.3fc3b912.woff2"
+  },
+  {
+    "revision": "d8ae6992e154ff72575eade8bf41991a",
+    "url": "/my-portfolio/static/media/montserrat-lightitalic-webfont.d8ae6992.woff"
+  },
+  {
+    "revision": "2fc5c881d63bb2751a76d2ebde141a19",
+    "url": "/my-portfolio/static/media/montserrat-regular-webfont.2fc5c881.woff2"
+  },
+  {
+    "revision": "8bfbf84b94be655989bc2ffc367020dc",
+    "url": "/my-portfolio/static/media/montserrat-regular-webfont.8bfbf84b.woff"
+  },
+  {
+    "revision": "24cdb1227d76f8be403fe18823e9a192",
+    "url": "/my-portfolio/static/media/montserrat-semibold-webfont.24cdb122.woff2"
+  },
+  {
+    "revision": "802802fa371a9e7d74c8cd2ddb72ccbd",
+    "url": "/my-portfolio/static/media/montserrat-semibold-webfont.802802fa.woff"
+  },
+  {
+    "revision": "a72813e6e4a2c1cec22e0c466c1e177a",
+    "url": "/my-portfolio/static/media/montserrat-semibolditalic-webfont.a72813e6.woff"
+  },
+  {
+    "revision": "d0da9cdf67d4075e521dddfde4fcbf26",
+    "url": "/my-portfolio/static/media/montserrat-semibolditalic-webfont.d0da9cdf.woff2"
+  },
+  {
+    "revision": "42ae2bf3053d67da23aeb7e5da6d5a05",
+    "url": "/my-portfolio/static/media/ocs-inventory.42ae2bf3.png"
+  },
+  {
+    "revision": "41f8f13b26a3280553cb287126b59b03",
+    "url": "/my-portfolio/static/media/square-solitaire.41f8f13b.png"
+  }
+]);
